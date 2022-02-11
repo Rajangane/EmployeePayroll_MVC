65850b037ffb95b4bbbdfaa9ff8af0fe431d65ba
Rajangane <pratiksharajangane7@gmail.com>
2022-02-11 14:24:44 +0530

    [Pratiksha] ADD : Adding Employee_Payroll MVC Project

---
+++ EmployeePayroll_MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,105 @@
+﻿using EmployeePayroll_MVC.Manager;
+using EmployeePayroll_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        Repository repository = new Repository();
+
+        public IActionResult GetEmployee()
+        {
+            List<EmployeeModel> emplist = new List<EmployeeModel>();
+            emplist = repository.GetAllEmployee().ToList();
+            return View(emplist);
+        }
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEmployee([Bind] EmployeeModel emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    repository.AddEmployee(emp);
+                    return RedirectToAction("GetEmployee");
+                }
+                return View(emp);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet]
+        public IActionResult UpdateEmployee(int? id)
+        {
+            if (id == null)
+            { 
+                return NotFound();
+            }
+            EmployeeModel employee = repository.GetAllEmployee().Where(e => e.EmployeeId == id).FirstOrDefault();
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult UpdateEmployee([Bind] EmployeeModel emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    repository.UpdateEmployee(emp);
+                    return RedirectToAction("GetEmployee");
+                }
+                return View(emp);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet]
+        public IActionResult DeleteEmployee(int? id)
+        {
+            EmployeeModel employee = repository.GetAllEmployee().Where(e => e.EmployeeId == id).FirstOrDefault();
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult DeleteEmployee(int id)
+        {
+            try
+            {
+                repository.DeleteEmployee(id);
+                return RedirectToAction("GetEmployee");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
+
+
+
---
+++ EmployeePayroll_MVC/Manager/Repository.cs
@@ -0,0 +1,136 @@
+﻿using EmployeePayroll_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_MVC.Manager
+{
+    public class Repository
+    {
+       
+        public string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Employees;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
+
+        SqlConnection con;
+        /// <summary>
+        /// Method For GetAllEmployees
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<EmployeeModel> GetAllEmployee()
+        {
+            List<EmployeeModel> empList = new List<EmployeeModel>();
+            using (con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    EmployeeModel emp = new EmployeeModel();
+                    emp.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                    emp.Name = rdr["Name"].ToString();
+                    emp.ProfileImage = rdr["ProfileImage"].ToString();
+                    emp.Gender = rdr["Gender"].ToString();
+                    emp.Department = rdr["Department"].ToString();
+                    emp.Salary = Convert.ToInt32(rdr["Salary"]);
+                    emp.StartDate = Convert.ToDateTime(rdr["StartDate"]);
+                    emp.Notes = rdr["Notes"].ToString();
+                    empList.Add(emp);
+                }
+                con.Close();
+            }
+            return empList;
+        }
+        /// <summary>
+        /// Adding Employees
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public bool AddEmployee(EmployeeModel employee)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("spAddEmployee", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@Name", employee.Name);
+                    cmd.Parameters.AddWithValue("@ProfileImage", employee.ProfileImage);
+                    cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                    cmd.Parameters.AddWithValue("@Department", employee.Department);
+                    cmd.Parameters.AddWithValue("@Salary", employee.Salary);
+                    cmd.Parameters.AddWithValue("@StartDate", employee.StartDate);
+                    cmd.Parameters.AddWithValue("@Notes", employee.Notes);
+                    con.Open();
+                    var result = cmd.ExecuteNonQuery();
+                    con.Close();
+                    if (result > 0)
+                        return true;
+                    else
+                        return false;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+        }
+        /// <summary>
+        /// Updating Employees
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public bool UpdateEmployee(EmployeeModel employee)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spUpdate", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmployeeId", employee.EmployeeId);
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@ProfileImage", employee.ProfileImage);
+                cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                cmd.Parameters.AddWithValue("@Department", employee.Department);
+                cmd.Parameters.AddWithValue("@Salary", employee.Salary);
+                cmd.Parameters.AddWithValue("@StartDate", employee.StartDate);
+                cmd.Parameters.AddWithValue("@Notes", employee.Notes);
+                con.Open();
+                var result = cmd.ExecuteNonQuery();
+                con.Close();
+                if (result > 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        /// <summary>
+        /// Deleting Employees
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool DeleteEmployee(int? id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmployeeId", id);
+                con.Open();
+                var result = cmd.ExecuteNonQuery();
+                con.Close();
+                if (result > 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+    }
+
+}
